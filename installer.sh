235f16fed1f1d32ae3ec2eec3c98075fa767dde0
Network Chunk <mhmiraj2005@gmail.com>
2025-08-14 00:47:19 +0600

    cli complete

--- installer.sh
+++ installer.sh
@@ -3,123 +3,89 @@
 # Function to check if .git exists in a directory
 check_git() {
     local dir=$1
-    if [ -f "$dir/README.md" ]; then           # this was easy to think for me, haha !! i know, akward logic
-        #echo "Debug: Found in $PWD/$dir"
-        return 0
-    else
-        #echo "Debug: not Found in $PWD/$dir"
-        return 1
-    fi
+    [ -d "$dir/.git" ] && return 0 || return 1
 }
 
 # Function to download a single dotfile repository
 download_dotfile() {
     local folder=$1
     local repo_url=$2
-    local temp_dir="temp_$(date +%s)"
+    #local temp_dir="temp_$(date +%s)"
+
+    if [ -d "$folder" ] && [ "$(ls -A "$folder")" ]; then
+        echo "Error: Directory $folder is not empty. Skipping clone."
+        return 1
+    fi
     
     echo "Downloading $folder dotfiles from $repo_url..."
     
-    # Create temporary directory
-    mkdir -p "$temp_dir"
-    
-    # Download the tarball
-    if curl -L --fail "$repo_url/archive/refs/heads/main.tar.gz" -o "$temp_dir/repo.tar.gz"; then
-        # Verify tarball is a valid gzip file
-        if file "$temp_dir/repo.tar.gz" | grep -q "gzip compressed data"; then
-            # Extract tarball
-            if tar -xzf "$temp_dir/repo.tar.gz" -C "$temp_dir"; then
-                # Find the extracted folder (GitHub appends branch name, e.g., Kitty-main)
-                extracted_folder=$(ls "$temp_dir" | grep -E '.*-main$')
-                
-                if [ -d "$temp_dir/$extracted_folder" ]; then
-                    # Create target folder if it doesn't exist
-                    mkdir -p "$folder"
-                    
-                    # Move contents (not the folder itself) to the target folder
-                    mv "$temp_dir/$extracted_folder/"* "$folder/" 2>/dev/null || {
-                        echo "Error: Failed to move contents to $folder."
-                        rm -rf "$temp_dir"
-                        return 1
-                    }
-                    
-                    echo "$folder dotfiles downloaded successfully."
-                else
-                    echo "Error: Could not find extracted folder in $temp_dir."
-                    rm -rf "$temp_dir"
-                    return 1
-                fi
-            else
-                echo "Error: Failed to extract tarball."
-                rm -rf "$temp_dir"
-                return 1
-            fi
-        else
-            echo "Error: Downloaded file is not a valid gzip tarball."
-            rm -rf "$temp_dir"
-            return 1
-        fi
-        # Clean up
-        rm -rf "$temp_dir"
+    mkdir -p "$folder"
+    cd "$folder"
+    if git clone "$repo_url" .;then
+        local error=false
+        echo "downloaded contents from $repo_url successfully"
     else
-        echo "Error: Failed to download repository from $repo_url."
-        rm -rf "$temp_dir"
-        return 1
+        local error=true
+        echo "contents from $repo_url couldn't be downloaded !!"
     fi
+    cd ..
+    if $error;then return 1;else return 0;fi
 }
 
 # Function to show download menu
 download_menu() {
-    echo "Download Dotfiles Menu"
-    echo "----------------------"
-    echo "1. Download Neovim dotfiles"
-    echo "2. Download Kitty dotfiles"
-    echo "3. Download Tmux dotfiles"
-    echo "4. Download Bash dotfiles"
-    echo "5. Download all dotfiles"
-    echo "[x]. Back to main menu : choose 'x' to return"
-    echo ""
-    
-    read -p "Enter your choice: " choice
-    echo ""
-    
-    case $choice in
-        1)
-            # @warning  it will only work if you provide the repo url without the ".git"
-            #[ e.g. "https://github.com/Miraj13123/Neovim" instead of "https://github.com/Miraj13123/Neovim.git" ]
-            download_dotfile "neovim" "https://github.com/Miraj13123/Neovim"
-            download_menu
-            ;;
-        2)
-            download_dotfile "kitty" "https://github.com/Miraj13123/Kitty"
-            download_menu
-            ;;
-        3)
-            download_dotfile "tmux" "https://github.com/Miraj13123/tmux"
-            download_menu
-            ;;
-        4)
-            download_dotfile "bash" "https://github.com/Miraj13123/Bash"
-            download_menu
-            ;;
-        5)
-            download_dotfile "neovim" "https://github.com/Miraj13123/Neovim.git"
-            download_dotfile "kitty" "https://github.com/Miraj13123/Kitty.git"
-            download_dotfile "tmux" "https://github.com/Miraj13123/tmux.git"
-            download_dotfile "bash" "https://github.com/Miraj13123/Bash.git"
-            download_menu
-            ;;
-        x|X)
-            clear
-            #show_menu
-            ;;
-        *)
-            echo "================================="
-            echo "Invalid choice, please try again."
-            echo "================================="
-            download_menu
-            ;;
-    esac
+    while true;do
+        echo "Download Dotfiles Menu"
+        echo "----------------------"
+        echo "1. Download oh-my-posh dotfiles"
+        echo "2. Download Neovim dotfiles"
+        echo "3. Download Kitty dotfiles"
+        echo "4. Download Tmux dotfiles"
+        echo "5. Download Bash dotfiles"
+        echo "6. Download all dotfiles"
+        echo "[x]. Back to main menu : choose 'x' to return"
+        echo "----------------------"
+        
+        read -p "Enter your choice: " choice
+        echo ""
+        
+        case $choice in
+            1)
+                # @warning  it will only work if you provide the repo url without the ".git"
+                #[ e.g. "https://github.com/Miraj13123/Neovim" instead of "https://github.com/Miraj13123/Neovim.git" ]
+                download_dotfile "omp" "https://github.com/Miraj13123/omp.git"
+                ;;
+            2)
+                download_dotfile "neovim" "https://github.com/Miraj13123/Neovim.git"
+                ;;
+            3)
+                download_dotfile "kitty" "https://github.com/Miraj13123/Kitty.git"
+                ;;
+            4)
+                download_dotfile "tmux" "https://github.com/Miraj13123/Tmux.git"
+                ;;
+            5)
+                download_dotfile "bash" "https://github.com/Miraj13123/Bash.git"
+                ;;
+            6)
+                download_dotfile "neovim" "https://github.com/Miraj13123/Neovim.git"
+                download_dotfile "kitty" "https://github.com/Miraj13123/Kitty.git"
+                download_dotfile "tmux" "https://github.com/Miraj13123/Tmux.git"
+                download_dotfile "bash" "https://github.com/Miraj13123/Bash.git"
+                download_dotfile "omp" "https://github.com/Miraj13123/omp.git"
+                ;;
+            x|X)
+                clear
+                break
+                #show_menu
+                ;;
+            *)
+                echo "================================="
+                echo "Invalid choice, please try again."
+                echo "================================="
+                ;;
+        esac
+    done
 }
 
 # Function to trigger download menu
@@ -134,7 +100,8 @@ run_installer() {
     
     if [ -f "$installer_script" ]; then
         echo "Running installer for $dir..."
-        bash "$installer_script"
+        bash "$installer_script" || { echo "Error: Installer script $installer_script failed"; return 1; }
+        return 0
     else
         echo "Error: Installer script $installer_script not found!"
         return 1
@@ -144,7 +111,7 @@ run_installer() {
 # Function to display info
 show_info() {
     echo "Dotfiles Installer"
-    echo "This script manages the installation of dotfiles for Neovim, Kitty, Tmux, and Bash."
+    echo "This script manages the installation of dotfiles for Oh-My-Posh, Neovim, Kitty, Tmux, and Bash."
     echo "Ensure dotfiles are downloaded before running installation options."
     echo "Repository: [Your GitHub repo URL here]"
 }
@@ -152,62 +119,43 @@ show_info() {
 # Function to display the menu
 show_menu() {
     # Check .git presence for each directory
-    local neovim_git=false
-    local kitty_git=false
-    local tmux_git=false
-    local bash_git=false
+    local neovim_git="(dotfiles aren't downloaded)"
+    local kitty_git="(dotfiles aren't downloaded)"
+    local tmux_git="(dotfiles aren't downloaded)"
+    local bash_git="(dotfiles aren't downloaded)"
+    local omp_git="(dotfiles aren't downloaded)"
     
     # calling function to check .git file
-    check_git "neovim" && neovim_git=true
-    check_git "kitty" && kitty_git=true
-    check_git "tmux" && tmux_git=true
-    check_git "bash" && bash_git=true
+    check_git "neovim" && neovim_git="(dotfiles are downloaded)"
+    check_git "kitty" && kitty_git="(dotfiles are downloaded)"
+    check_git "tmux" && tmux_git="(dotfiles are downloaded)"
+    check_git "bash" && bash_git="(dotfiles are downloaded)"
+    check_git "omp" && omp_git="(dotfiles are downloaded)"
     
     # Check if all dotfiles are downloaded
-    local all_downloaded=true
-    if ! $neovim_git || ! $kitty_git || ! $tmux_git || ! $bash_git; then
-        all_downloaded=false
-    fi
+    local all_downloaded="(all dotfiles are downloaded)"
+    for dir in neovim kitty tmux bash omp; do
+        if ! check_git "$dir"; then
+            all_downloaded="(all dotfiles aren't downloaded)"
+            break
+        fi
+    done
 
     echo "Dotfiles Installer Menu"
     echo "----------------------"
     echo "0. Download dotfiles"
-    
-    if $all_downloaded; then
-        echo "1. Install all"
-    else
-        echo "1. Install all (dotfiles not fully downloaded)"
-    fi
-    
-    if $neovim_git; then
-        echo "2. Install Neovim dots"
-    else
-        echo "2. Install Neovim dots (dotfiles not downloaded)"
-    fi
-    
-    if $kitty_git; then
-        echo "3. Install Kitty dots"
-    else
-        echo "3. Install Kitty dots (dotfiles not downloaded)"
-    fi
-    
-    if $tmux_git; then
-        echo "4. Install Tmux dots"
-    else
-        echo "4. Install Tmux dots (dotfiles not downloaded)"
-    fi
-    
-    if $bash_git; then
-        echo "5. Install Bash dots"
-    else
-        echo "5. Install Bash dots (dotfiles not downloaded)"
-    fi
-    
-    echo "6. Info"
+    echo "1. Install all, $all_downloaded"
+    echo "2. Install oh-my-posh dots, $omp_git"
+    echo "3. Install Neovim dots, $neovim_git"
+    echo "4. Install Kitty dots, $kitty_git"
+    echo "5. Install Tmux dots, $tmux_git"
+    echo "6. Install Bash dots, $bash_git"
+    echo "7. Info"
     echo "[x]. Exit : choose 'x' to exit"
     echo ""
     
     # Read user input
+    local choice=""
     read -p "Enter your choice: " choice
     echo ""
 
@@ -218,48 +166,57 @@ show_menu() {
         show_menu
     elif [ "$choice" = "1" ]; then
         clear
-        if $all_downloaded; then
+        if [[ "$all_downloaded"=="(all dotfiles are downloaded)" ]]; then
             run_installer "neovim"
             run_installer "kitty"
             run_installer "tmux"
             run_installer "bash"
+            run_installer "omp"
         else
             echo "Dotfiles aren't fully downloaded. Please download dotfiles to continue."
         fi
         show_menu
     elif [ "$choice" = "2" ]; then
         clear
-        if $neovim_git; then
+        if [[ "$omp_git"=="(dotfiles are downloaded)" ]]; then
+            run_installer "omp"
+        else
+            echo "oh-my-posh dotfiles aren't available. Please download dotfiles to continue."
+        fi
+        show_menu
+    elif [ "$choice" = "3" ]; then
+        clear
+        if [[ "$neovim_git"=="(dotfiles are downloaded)" ]]; then
             run_installer "neovim"
         else
             echo "Neovim dotfiles aren't available. Please download dotfiles to continue."
         fi
         show_menu
-    elif [ "$choice" = "3" ]; then
+    elif [ "$choice" = "4" ]; then
         clear
-        if $kitty_git; then
+        if [[ "$kitty_git"=="(dotfiles are downloaded)" ]]; then
             run_installer "kitty"
         else
             echo "Kitty dotfiles aren't available. Please download dotfiles to continue."
         fi
         show_menu
-    elif [ "$choice" = "4" ]; then
+    elif [ "$choice" = "5" ]; then
         clear
-        if $tmux_git; then
+        if [[ "$tmux_git"=="(dotfiles are downloaded)" ]]; then
             run_installer "tmux"
         else
             echo "Tmux dotfiles aren't available. Please download dotfiles to continue."
         fi
         show_menu
-    elif [ "$choice" = "5" ]; then
+    elif [ "$choice" = "6" ]; then
         clear
-        if $bash_git; then
+        if [[ "$bash_git"=="(dotfiles are downloaded)" ]]; then
             run_installer "bash"
         else
             echo "Bash dotfiles aren't available. Please download dotfiles to continue."
         fi
         show_menu
-    elif [ "$choice" = "6" ]; then
+    elif [ "$choice" = "7" ]; then
         clear
         show_info
         show_menu
